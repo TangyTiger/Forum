0d7f71b1c6cde090560bbd13e1016072cabe38a5
TangyTiger <shahaditya@outlook.com>
2022-01-23 12:37:34 -0800

    i think finished frontend??

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-//Navbar
-function showNavBar(){
+
+//NavBar
+function hideIconBar(){
     var iconBar = document.getElementById("iconBar");
     var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:none");
+    iconBar.setAttribute("style", "display:none;");
     navigation.classList.remove("hide");
 }
 
-function hideNavBar(){
+function showIconBar(){
     var iconBar = document.getElementById("iconBar");
     var navigation = document.getElementById("navigation");
-    iconBar.setAttribute("style", "display:block");
+    iconBar.setAttribute("style", "display:block;");
     navigation.classList.add("hide");
+}
 
+//Comment
+function showComment(){
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.remove("hide");
+}
+
+//Reply
+function showReply(){
+    var replyArea = document.getElementById("reply-area");
+    replyArea.classList.remove("hide");
 }
